3b5909f21abf9b6b2b180cf4f7b22cfe49562fa8
Naman Sharma <nmnsharma20@gmail.com>
2020-05-23 19:27:00 +0530

    Add solution for problem `A - Expression` Link: https://codeforces.com/contest/479/problem/A Tags: brute force, math, *1000 Ref: https://codeforces.com/contest/479/submission/81019659

---
+++ codeforces/479/A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin >> a >> b >> c;
+    int ans = max(a*b+c,(a+b)*c);
+    ans = max(a*b*c,ans);
+    ans = max(ans,a+b+c);
+    ans = max(ans,a*(b+c));
+    ans = max(ans,a+b*c);
+    cout << ans << "\n";
+}
