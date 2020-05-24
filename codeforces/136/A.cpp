9d79d430c019a762f157438b85df7d407e74333e
Naman Sharma <nmnsharma20@gmail.com>
2020-05-24 16:59:00 +0530

    Add solution for problem `A - Presents` Link: https://codeforces.com/contest/136/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/136/submission/81170771

---
+++ codeforces/136/A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> ans(n+1);
+	int arr[n+1];
+	for(int i=1;i<=n;i++)
+	{
+		cin >> arr[i];
+		ans[arr[i]] = i;
+	}
+	for(int i=1;i<=n;i++)
+		cout << ans[i] << " "; 
+}
