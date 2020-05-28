caeede32058ddf23f2e34facffb77924acf18b7c
Naman Sharma <nmnsharma20@gmail.com>
2020-05-28 13:19:00 +0530

    Add solution for problem `A - Holiday Of Equality` Link: https://codeforces.com/contest/758/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/758/submission/81682440

---
+++ codeforces/758/A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+		cin >> arr[i];
+	sort(arr,arr+n);
+	int ans = 0;
+	for(int i=0;i<n-1;i++)
+		 ans += arr[n-1]-arr[i];
+	cout << ans << "\n";
+}
