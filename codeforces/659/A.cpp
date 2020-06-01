0a67607a43b330ce0e52ef638ec74dd115182076
Naman Sharma <nmnsharma20@gmail.com>
2020-06-01 16:25:00 +0530

    Add solution for problem `A - Round House` Link: https://codeforces.com/contest/659/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/659/submission/82207655

--- codeforces/659/A.cpp
+++ codeforces/659/A.cpp
@@ -4,9 +4,9 @@ int main()
 {
     int n,a,b;
     cin >> n >> a >> b;
-    int pos_change = abs(b)%n;
+    int pos_change = b%n;
     if(b >= 0)
         cout << (a-1+pos_change)%n+1;
     else
-        cout << (a-1-pos_change+n)%n +1;
+        cout << ((a-1+pos_change)%n+n)%n +1;
 }
