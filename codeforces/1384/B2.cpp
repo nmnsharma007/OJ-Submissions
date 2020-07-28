2f41b09cee2c5486b9019ef3b1aa2947cd082e62
Naman Sharma <nmnsharma20@gmail.com>
2020-07-28 22:19:00 +0530

    Add solution for problem `B2 - Koa and the Beach (Hard Version)` Link: https://codeforces.com/contest/1384/problem/B2 Tags: constructive algorithms, dp, greedy, implementation, *2200 Ref: https://codeforces.com/contest/1384/submission/88223716

---
+++ codeforces/1384/B2.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while(t--){
+		int n;
+		cin >> n;
+		ll k,l;
+		cin >> k >> l;
+		ll depth[n+1];
+		vector<int> safe;
+		safe.push_back(0);
+		for(int i = 1; i <= n;++i){
+			cin >> depth[i];
+			if(depth[i] + k <= l)
+				safe.push_back(i);
+		}
+		safe.push_back(n+1);
+		bool ok = true;
+		for(int i = 1; i < (int)safe.size()&&ok;++i){
+			bool down = true;
+			ll tide = k;
+			for(int j = safe[i-1]+1; j < safe[i];++j){
+				tide += down ? -1 : 1;
+				if(l < depth[j]){
+					ok = false;
+					break;
+				}
+				if(tide + depth[j] > l && down == false){
+					ok = false;
+					break;
+				}
+				if(tide + depth[j] > l)
+					tide = l-depth[j];
+				if(tide == 0)
+					down = false;
+				else if(tide == k)
+					down = true;
+			}
+		}
+		if(ok)
+			cout << "YES\n";
+		else
+			cout << "NO\n";
+	}
+}
