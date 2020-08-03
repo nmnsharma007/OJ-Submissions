c828df152b568314798391e8e679f58602bf8ce2
Naman Sharma <nmnsharma20@gmail.com>
2020-08-03 19:11:00 +0530

    Add solution for problem `C - The Child and Toy` Link: https://codeforces.com/contest/437/problem/C Tags: graphs, greedy, sortings, *1400 Ref: https://codeforces.com/contest/437/submission/88803548

--- codeforces/437/C.cpp
+++ codeforces/437/C.cpp
@@ -5,15 +5,28 @@ int main()
 {
 	int n,m;
 	cin >> n >> m;
+	vector<pair<int,int>> edges,cost(n+1);
 	int arr[n+1];
 	for(int i = 1; i <= n;++i){
 		cin >> arr[i];
+		cost[i] = {arr[i],i};
+	}
+	for(int i = 0; i < m;++i){
+		int x,y;
+		cin >> x >> y;
+		edges.push_back({x,y});
 	}
 	int ans = 0;
-	for(int i = 0; i < m;++i){
-		int u,v;
-		cin >> u >> v;
-		ans += min(arr[u],arr[v]);
+	sort(cost.begin()+1,cost.end());
+	for(int i = n; i > 0;--i){
+		int val = cost[i].first;
+		int node = cost[i].second;
+		for(int j = 0; j < m;++j){
+			if(edges[j].second == node || edges[j].first == node){
+				ans += (edges[j].first != node) ? arr[edges[j].first] : arr[edges[j].second];
+				edges[j] = {-1,-1};
+			}
+		}
 	}
 	cout << ans << "\n";
 }
