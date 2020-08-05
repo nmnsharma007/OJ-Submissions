3e4004ed732b1426740a6c34c3f169327c8a5c07
Naman Sharma <nmnsharma20@gmail.com>
2020-08-05 16:09:00 +0530

    Add solution for problem `B - Polo the Penguin and Matrix` Link: https://codeforces.com/contest/289/problem/B Tags: brute force, dp, implementation, sortings, ternary search, *1400 Ref: https://codeforces.com/contest/289/submission/88937217

--- codeforces/289/B.cpp
+++ codeforces/289/B.cpp
@@ -7,32 +7,34 @@ int main()
 	cin.tie(0);
 	int n,m,d;
 	cin >> n >> m >> d;
-	int matrix[n*m+1];
-	int k = 1;
+	int matrix[n][m];
 	for(int i = 0; i < n;++i){
 		for(int j = 0; j < m;++j){
-			int x;
-			cin >> x;
-			matrix[k++] = x;
+			cin >> matrix[i][j];
 		}
 	}
-	int flag = 1;
-	sort(matrix+1,matrix+n*m+1);
-	for(int i = 2; i <= m*n;++i){
-		int prev = matrix[i-1]%d;
-		int rem = matrix[i]%d;
-		if(prev != rem)
-			flag = 0;
+	int ans = 1e9+7;
+	int flag = 0;
+	for(int i = 1; i <= 1e4;++i){
+		bool status = true;
+		int temp = 0;
+		for(int row = 0; row < n;++row){
+			for(int col = 0; col < m;++col){
+				if(abs(matrix[row][col] - i)%d){
+					status = false;
+					break;
+				}
+				temp += abs((matrix[row][col]-i))/d;
+			}
+			if(status == false)
+				break;
+		}
+		if(status){
+			ans = min(ans,temp);
+			flag = 1;
+		}
 	}
-	int ans;
 	if(flag == 0)
 		ans = -1;
-	else{
-		ans = 0;
-		int target = matrix[(m*n)/2 + 1];
-		for(int i = 1; i <= m*n;++i){
-			ans += abs(matrix[i]-target)/d;
-		}
-	}
 	cout << ans << "\n";
 }
