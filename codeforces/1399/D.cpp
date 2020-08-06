59c9d834be6d14a5705cafac90c680f0eeeaee4e
Naman Sharma <nmnsharma20@gmail.com>
2020-08-06 16:03:00 +0530

    Add solution for problem `D - Binary String To Subsequences` Link: https://codeforces.com/contest/1399/problem/D Tags: constructive algorithms, data structures, greedy, implementation, *1500 Ref: https://codeforces.com/contest/1399/submission/89110940

---
+++ codeforces/1399/D.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while(t--){
+		int n;
+		cin >> n;
+		string s;
+		cin >> s;
+		stack<int> q[2];
+		int ans[n];
+		int k = 0;
+		for(int i = 0; i < n;++i){
+			int d = s[i] - '0';
+			if(!q[d].empty()){
+				int val = q[d].top();
+				q[d].pop();
+				ans[i] = val;
+				q[1-d].push(val);
+			}
+			else{
+				q[1-d].push(++k);
+				ans[i] = k;
+			}
+		}
+		cout << k << "\n";
+		for(auto x : ans)
+			cout << x << " ";
+		cout << "\n";
+	}
+}
