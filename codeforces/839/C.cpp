652e62563b869ac3ff3f3701a00ddd22d8f96e7d
Naman Sharma <nmnsharma20@gmail.com>
2020-08-14 23:24:00 +0530

    Add solution for problem `C - Journey` Link: https://codeforces.com/contest/839/problem/C Tags: dfs and similar, dp, graphs, probabilities, trees, *1500 Ref: https://codeforces.com/contest/839/submission/89963427

--- codeforces/839/C.cpp
+++ codeforces/839/C.cpp
@@ -3,21 +3,23 @@ using namespace std;
 using ll = long long;
 const int N = 1e5+5;
 vector<int> adj[N];
-vector<int> dp(N);
-vector<double> prob(N);
+bool visited[N];
+vector<double> dp(N);
 void dfs(int cur_node,int parent)
 {
-	double k = adj[cur_node].size();
-	if(parent != -1){
-		dp[cur_node] = 1 + dp[parent];
+	visited[cur_node] = true;
+	int k;
+	if(parent == -1)
+		k = adj[cur_node].size();
+	else
 		k = adj[cur_node].size()-1;
-	}
+	dp[cur_node] = 0.0;
 	for(int i = 0; i < (int)adj[cur_node].size();++i){
 		int next_node = adj[cur_node][i];
-		if(parent == next_node)
+		if(next_node == parent)
 			continue;
-		prob[next_node] = prob[cur_node] * (double)(1/k);
 		dfs(next_node,cur_node);
+		dp[cur_node] = dp[cur_node] + (double)(dp[next_node]+1)/(double)k;
 	}
 }
 int main()
@@ -30,14 +32,8 @@ int main()
 		adj[u].push_back(v);
 		adj[v].push_back(u);
 	}
+	memset(visited,false,sizeof(visited));
 	dp[1] = 0;
-	prob[1] = 1.0;
 	dfs(1,-1);
-	double ans = 0.0;
-	for(int i = 2; i <= n;++i){
-		if(adj[i].size() == 1){
-			ans = ans + dp[i] * prob[i];
-		}
-	}
-	printf("%.12lf\n",ans);
+	printf("%.12lf\n",dp[1]);
 }
