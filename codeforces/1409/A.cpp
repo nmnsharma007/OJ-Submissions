0e15022f3694a7d890bb934e43c6767e0812e449
Naman Sharma <nmnsharma20@gmail.com>
2020-09-05 15:06:00 +0530

    Add solution for problem `A - Yet Another Two Integers Problem` Link: https://codeforces.com/contest/1409/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1409/submission/91926810

---
+++ codeforces/1409/A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while(t--){
+		int a,b;
+		cin >> a >> b;
+		int ans = abs(b-a)/10 + ((abs(b-a)%10) ? 1 : 0);
+		cout << ans << "\n";
+	}
+}
