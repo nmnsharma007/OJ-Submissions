723f0e747b07d8a51359aafac796bed03d4ceb9b
Naman Sharma <nmnsharma20@gmail.com>
2020-10-17 18:46:00 +0530

    Add solution for problem `A - Box is Pull` Link: https://codeforces.com/contest/1428/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1428/submission/95748029

---
+++ codeforces/1428/A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while(t--){
+		ll x1,y1,x2,y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		if(x1 == x2 || y1 == y2)
+			cout << abs(x1-x2) + abs(y1-y2) << "\n";
+		else
+			cout << abs(x1-x2) + abs(y1-y2) + 2 << "\n";
+	}
+}
