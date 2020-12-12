25a0ae2878e90b4fcab31ca9343d1fc49271fa87
Naman Sharma <nmnsharma20@gmail.com>
2020-12-12 18:38:00 +0530

    Add solution for problem `E - Number of Simple Paths` Link: https://codeforces.com/contest/1454/problem/E Tags: combinatorics, dfs and similar, graphs, trees, *2000 Ref: https://codeforces.com/contest/1454/submission/101021831

---
+++ codeforces/1454/E.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while(t--){
+		int n;
+		cin >> n;
+		set<int> adj[n+1];
+		for(int i = 0; i < n;++i){
+			int u,v;
+			cin >> u >> v;
+			adj[u].insert(v);
+			adj[v].insert(u);
+		}
+		stack<int> s;
+		for(int i = 1; i <= n;++i){
+			if(adj[i].size() == 1)
+				s.push(i);
+		}
+		vector<ll> cnt(n+1,1);
+		while(!s.empty()){
+			int cur_node = s.top();
+			s.pop();
+			if(adj[cur_node].size() != 1)
+				continue;
+			int parent = *adj[cur_node].begin();
+			adj[parent].erase(cur_node);
+			adj[cur_node].clear();
+			cnt[parent] += cnt[cur_node];
+			s.push(parent);
+		}
+		ll ans = 0;
+		for(int node = 1; node <= n;++node){
+			if(adj[node].size() != 2)
+				continue;
+			ans += (cnt[node]*(cnt[node]-1))/2LL + cnt[node] * ((ll)n-cnt[node]);
+		}
+		cout << ans << "\n";
+	}
+}
