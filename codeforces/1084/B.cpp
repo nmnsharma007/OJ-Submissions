63bd7b6bbfb57735f8c2f62f9dcf1af79b4fc7f3
Naman Sharma <nmnsharma20@gmail.com>
2020-12-14 15:57:00 +0530

    Add solution for problem `B - Kvass and the Fair Nut` Link: https://codeforces.com/contest/1084/problem/B Tags: greedy, implementation, *1200 Ref: https://codeforces.com/contest/1084/submission/101160212

--- codeforces/1084/B.cpp
+++ codeforces/1084/B.cpp
@@ -2,45 +2,37 @@
 using namespace std;
 using ll = long long;
 const int N = 1e3+5;
-vector<ll> v(N);
-bool is_possible(ll amount,ll n,ll s)
-{
-	ll total = 0;
-	for(int i = 1; i <= n;++i){
-		if(v[i] < amount)
-			return false;
-		total += v[i]-amount;
-		if(total >= s)
-			return true;
-	}
-	return false;
-}
+
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	ll n,s;
 	cin >> n >> s;
-	ll sum = 0,mini = 1e9+7;
+	vector<ll> v(n+1);
+	ll sum = 0,mini = 1e9+7;;
 	for(int i = 1; i <= n;++i){
 		cin >> v[i];
 		sum += v[i];
+		mini = min(mini,v[i]);
 	}
-	sort(v.begin()+1,v.begin()+n+1);
 	if(sum < s)
 		cout << "-1\n";
 	else{
-		ll left = 0,right = 1e9+10;
-		ll ans = 0;
-		while(left <= right){
-			ll mid = (left+right)/2LL;
-			if(is_possible(mid,n,s)){
-				ans = max(ans,mid);
-				left = mid+1;
-			}
-			else
-				right = mid-1;
+		ll taken = 0;
+		for(int i = 1; i <= n;++i){
+			taken += v[i]-mini;
+			v[i] = mini;
 		}
-		cout << ans << "\n";
+		if(taken >= s)
+			cout << mini << "\n";
+		else{
+			s -= taken;
+			ll factor = s/n;
+			if(s <= factor*n)
+				cout << mini-factor << "\n";
+			else
+				cout << mini-factor-1 << "\n";
+		}
 	}
 }
